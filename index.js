f5b5a74954a3e5311dd406cebaf40572df940e11
Evan SandersonE <evan.j.sanderson@gmail.coM>
2016-06-30 21:39:10 -0400

    did validation and added comments

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-
+// each of these Jquery click functions animates the transition to the corresponding div
 $("#middle_button").click(function() {
     $('html,body').animate({
         scrollTop: $("#middle").offset().top - 50},
