1227b736baac813f70e2b8d74c2b951202e114f6
Evan SandersonE <evan.j.sanderson@gmail.coM>
2016-07-06 22:29:21 -0400

    implemented two buttons and reformatted

--- index.js
+++ index.js
@@ -2,27 +2,37 @@
 // each of these Jquery click functions animates the transition to the corresponding div
 $("document").ready(function() {
 
-  var btn = $("#aboutMeClick");
-  var div = $(".aboutMe");
-  btn.on("click", function(){
+  var btn1 = $("#aboutMeClick");
+  var aboutMe = $(".aboutMe");
+  btn1.on("click", function(){
     console.log("button working");
-    div.toggleClass("slide-left");
+    aboutMe.toggleClass("slide-left");
 
   })
 
-$("#middle_button").click(function() {
+  var btn2 = $("#codingClick");
+  var codingBackground = $(".codingBackground");
+  btn2.on("click",function(){
+    console.log("button working");
+    codingBackground.toggleClass("slide-right");
+  })
+
+$("#middle_button").click(function(e) {
+    e.preventDefault();
     $('html,body').animate({
         scrollTop: $("#middle").offset().top - 50},
         'slow');
 });
 
-$("#bottom_button").click(function() {
+$("#bottom_button").click(function(e) {
+    e.preventDefault();
     $('html,body').animate({
         scrollTop: $("#bottom").offset().top - 50},
         'slow');
 });
 
-$("#top_button").click(function() {
+$("#top_button").click(function(e) {
+    e.preventDefault();
     $('html,body').animate({
         scrollTop: $("#background").offset().top - 50},
         'slow');
