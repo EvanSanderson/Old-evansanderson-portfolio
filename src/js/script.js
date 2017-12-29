0ec25dd6a9bc478d154e14099cba9358f0979157
Evan Sanderson <evan.j.sanderson@gmail.com>
2017-12-29 13:43:28 -0500

    makig first paneling

--- src/js/script.js
+++ src/js/script.js
@@ -2,28 +2,37 @@ $(document).ready(function () {
     var controller = new ScrollMagic.Controller();
 
     var wipeAnimation = new TimelineMax()
-        .fromTo("section.panel.turqoise", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone})
-        .fromTo("section.panel.green", 1, { x: "100%" }, { x: "0%", ease: Linear.easeNone })
-        .fromTo("section.panel.bordeaux", 1, { y: "-100%" }, { y: "0%", ease: Linear.easeNone });
+        .fromTo("div.panel.lilac", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone})
+        .fromTo("div.panel.green", 1, { x: "100%" }, { x: "0%", ease: Linear.easeNone })
+        .fromTo("div.panel.grey", 1, { y: "-100%" }, { y: "0%", ease: Linear.easeNone });
     
     var scene = new ScrollMagic.Scene({ 
-        triggerElement: "#pinContainer",
+        triggerElement: "#whoami",
         triggerHook: "onLeave",
         duration: "300%"
      })
-        .setPin('#pinContainer')
+        .setPin('#whoami')
         .setTween(wipeAnimation)
         .addTo(controller);
         
         var circle = $("circle#circle");
-        var newTween = new TimelineMax()
-                .add(TweenMax.to(circle, 10, {fill: "orange", ease: Linear.easeNone}))
-                .add(TweenMax.to(circle, 10, {stroke: "green", ease: Linear.easeNone}));
+        var circleTween = new TimelineMax()
+                .add(TweenMax.to(circle, 5, {strokeDashoffset: 0, ease: Linear.easeNone, repeat:-1}))
                 
-        var scene2 = new ScrollMagic.Scene({triggerElement: "#section-3", duration: 200, tweenChanges: true})
-                        .setTween(newTween)
+        var scene2 = new ScrollMagic.Scene({triggerElement: "#whoami", duration: 200, tweenChanges: true})
+                        .setTween(circleTween)
                         .addTo(controller);
-            
+
+        var shakeleft = $("path#shakeleft");
+        var shakeright = $("path#shakeright");
+
+        var shakeTween = new TimelineMax()
+                            .add(TweenMax.to(shakeleft, 3, {strokeDashoffset: 0, ease: Linear.easeNone, repeat: -1}))
+                            .add(TweenMax.to(shakeright, 5, {strokeDashoffset: 0, ease: Linear.easeNone, repeat: -1}));
+
+        var shakeScene = new ScrollMagic.Scene({triggerElement: "#section-3", duration: 200, tweenChanges:true})
+                        .setTween(shakeTween)
+                        .addTo(controller)
 });
 
 
